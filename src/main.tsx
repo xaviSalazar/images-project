e3dca0f351efe0b876eb98b7062d905c6d0a201c
xaviSalazar <xavicoel@gmail.com>
2024-05-06 22:08:52 +0200

    Feat: tooltip rovider, theme provider and utils

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import App from './App.tsx'
 import './globals.css'
+import { ThemeProvider } from "next-themes"
+import { TooltipProvider } from "./components/ui/tooltip.tsx"
+
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" disableTransitionOnChange>
+
+      <TooltipProvider>
+
     <App />
+    </TooltipProvider>
+
+    </ThemeProvider>
+
+    </QueryClientProvider>
   </React.StrictMode>,
 )
