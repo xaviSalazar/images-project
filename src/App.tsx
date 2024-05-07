77f5bdafa1bf6267259e30e45dd1e32e559f11f5
xaviSalazar <xavicoel@gmail.com>
2024-05-07 15:14:27 +0200

    Feat: added header, and states zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Toaster } from "./components/ui/toaster";
+import Header from "@/components/Header"
 
-import "./App.css";
 
 function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between w-full bg-[radial-gradient(circle_at_1px_1px,_#8e8e8e8e_1px,_transparent_0)] [background-size:20px_20px] bg-repeat">
       <Toaster />
+      <Header />
+
     </main>
   );
 }
