9df277a2bd3240b3feeccd4a7ed8448170a5db0c
xaviSalazar <xavicoel@gmail.com>
2024-06-14 22:18:22 +0200

    images scrolling options

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,47 @@ import { RotateCw, Image, Upload } from "lucide-react";
 import SettingsDialog from "./Settings";
 import LanguageSwitcher from "./LanguageSwitcher";
 import PromptInput from "./PromptInput"
+// import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { IconButton, ImageUploadButton } from "@/components/ui/button";
+
+// const images = [
+//   'https://via.placeholder.com/150',
+//   'https://via.placeholder.com/150',
+//   'https://via.placeholder.com/150',
+
+// ];
+
+// interface VerticalImageScrollProps {
+//   images: string[];
+// }
+
+// export function ScrollAreaImages(props: VerticalImageScrollProps) {
+//   const { images } = props;
+
+//   const handleImageClick = (src: string) => {
+//     // Perform the action you want when the image is clicked.
+//     // For example, you can log the image source.
+//     console.log('Image clicked:', src);
+//   };
+
+//   return (
+//     <ScrollArea className="h-72 w-48 rounded-md border overflow-y-auto">
+//       {images.map((src, index) => (
+//         <figure key={index} className="shrink-0">
+//           <div className="overflow-hidden rounded-md">
+//             <img
+//               src={src}
+//               alt={`image-${index}`}
+//               className="aspect-[3/4] h-fit w-fit object-cover cursor-pointer"
+//               onClick={() => handleImageClick(src)}
+//             />
+//           </div>
+//         </figure>
+//       ))}
+//     </ScrollArea>
+//   );
+// }
 
 const Header = () => {
   const [
@@ -63,6 +102,11 @@ const Header = () => {
         <Shortcuts />
         <SettingsDialog />
       </div>
+
+      {/* <div className="flex gap-3 absolute top-[150px] left-[24px] items-center">
+      <ScrollAreaImages images={images} />
+      </div>
+       */}
     </header>
   );
 };
