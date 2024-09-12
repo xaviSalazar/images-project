ca969d798a64cffda504483a324cbbfce772e40d
xaviSalazar <xavicoel@gmail.com>
2024-09-12 16:36:37 +0200

    FIX position for developemnt mode switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,10 @@ import Shortcuts from "@/components/Shortcuts";
 import SettingsDialog from "./Settings";
 import LanguageSwitcher from "./LanguageSwitcher";
 import PromptInput from "./PromptInput";
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+import { useStore } from "@/lib/states";
+
 // import { ScrollArea } from "@/components/ui/scroll-area";
 
 // import { IconButton, ImageUploadButton } from "@/components/ui/button";
@@ -80,6 +84,19 @@ const Header = () => {
   //   state.imageHeight,
   //   state.imageWidth,
   // ]);
+
+  const [
+    updateSettings,
+  ] = useStore((state) => [
+    state.updateSettings,
+  ]);
+
+  const dev_mode = useStore((state => state.settings.isDevModeActive))
+  const handleSwitchChange = (checked: boolean) => {
+    updateSettings({ isDevModeActive: checked });
+  };
+
+
   return (
     <header className="h-[60px] px-6 py-4 absolute top-[0] flex justify-between items-center w-full z-20 border-b backdrop-filter backdrop-blur-md bg-background/70">
       {/* <div className="flex items-center gap-1">
@@ -95,6 +112,18 @@ const Header = () => {
       </div> */}
 
       {/* {model.need_prompt ? <PromptInput /> : <></>} */}
+
+      <div className="flex items-center space-x-2">
+          <Switch 
+            id="development-mode" 
+            checked={dev_mode}
+            onCheckedChange={handleSwitchChange} 
+          />
+          <Label htmlFor="development-mode">
+          {dev_mode ? 'Testing mode' : 'Oficial mode'}
+          </Label>
+      </div>
+
       <PromptInput />
 
       <div className="flex gap-1">
--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -5,8 +5,6 @@ import { useClickAway, useToggle } from "react-use";
 import { Textarea } from "./ui/textarea";
 import { cn } from "@/lib/utils";
 import { ReloadIcon } from "@radix-ui/react-icons"
-import { Label } from "@/components/ui/label"
-import { Switch } from "@/components/ui/switch"
 
 const PromptInput = () => {
   const [
@@ -30,7 +28,6 @@ const PromptInput = () => {
   ]);
 
   const prompt = useStore((state) => state.settings.prompt);
-  const dev_mode = useStore((state => state.settings.isDevModeActive))
 
   const [showScroll, toggleShowScroll] = useToggle(false);
 
@@ -66,10 +63,6 @@ const PromptInput = () => {
     }
   };
 
-  const handleSwitchChange = (checked: boolean) => {
-    updateSettings({ isDevModeActive: checked });
-  };
-
   const onKeyUp = (e: React.KeyboardEvent) => {
     if (e.key === "Enter" && e.ctrlKey && prompt.length !== 0) {
       //  handleRepaintClick()
@@ -87,24 +80,12 @@ const PromptInput = () => {
   return (
     <div className="flex gap-4 relative w-full justify-center h-full">
       <div className="absolute flex gap-4">
-
-        <div className="flex items-center space-x-2">
-          <Switch 
-            id="development-mode" 
-            checked={dev_mode}
-            onCheckedChange={handleSwitchChange} 
-          />
-          <Label htmlFor="development-mode">
-          {dev_mode ? 'Testing mode' : 'Oficial mode'}
-          </Label>
-        </div>
-
-        <Button
-            variant="outline"
-            size="sm"
-            onClick={handleDescribeImg}
-            disabled={isInpainting}
-          >
+      <Button
+          variant="outline"
+          size="sm"
+          onClick={handleDescribeImg}
+          disabled={isInpainting}
+        >
         {isInpainting && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
           GENERATE PROMPT
         </Button>
