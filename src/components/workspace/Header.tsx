660e2ae7280f71232dfa4c8eb5193ebf10a90ae3
salazar <victor.salazar.external@atos.net>
2024-10-07 09:36:42 +0200

    Fix header with overflow y textarea

--- src/components/workspace/Header.tsx
+++ src/components/workspace/Header.tsx
@@ -3,7 +3,7 @@
 import Shortcuts from "@/components/workspace/Shortcuts";
 // import { useStore } from "@/lib/states";
 // import { RotateCw, Image, Upload } from "lucide-react";
-import SettingsDialog from "./Settings";
+// import SettingsDialog from "./Settings";
 import InstructionsDialog from "./Instructions";
 
 import LanguageSwitcher from "./LanguageSwitcher";
@@ -12,80 +12,7 @@ import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { useStore } from "@/lib/states";
 
-// import { ScrollArea } from "@/components/ui/scroll-area";
-
-// import { IconButton, ImageUploadButton } from "@/components/ui/button";
-
-// const images = [
-//   'https://via.placeholder.com/150',
-//   'https://via.placeholder.com/150',
-//   'https://via.placeholder.com/150',
-
-// ];
-
-// interface VerticalImageScrollProps {
-//   images: string[];
-// }
-
-// export function ScrollAreaImages(props: VerticalImageScrollProps) {
-//   const { images } = props;
-
-//   const handleImageClick = (src: string) => {
-//     // Perform the action you want when the image is clicked.
-//     // For example, you can log the image source.
-//     console.log('Image clicked:', src);
-//   };
-
-//   return (
-//     <ScrollArea className="h-72 w-48 rounded-md border overflow-y-auto">
-//       {images.map((src, index) => (
-//         <figure key={index} className="shrink-0">
-//           <div className="overflow-hidden rounded-md">
-//             <img
-//               src={src}
-//               alt={`image-${index}`}
-//               className="aspect-[3/4] h-fit w-fit object-cover cursor-pointer"
-//               onClick={() => handleImageClick(src)}
-//             />
-//           </div>
-//         </figure>
-//       ))}
-//     </ScrollArea>
-//   );
-// }
-
 const Header = () => {
-  // const [
-  //   file,
-  //   customMask,
-  //   isInpainting,
-  //   serverConfig,
-  //   runMannually,
-  //   enableUploadMask,
-  //   model,
-  //   setFile,
-  //   setCustomFile,
-  //   runInpainting,
-  //   showPrevMask,
-  //   hidePrevMask,
-  //   imageHeight,
-  //   imageWidth,
-  // ] = useStore((state) => [
-  //   state.file,
-  //   state.customMask,
-  //   state.isInpainting,
-  //   state.serverConfig,
-  //   state.runMannually(),
-  //   state.settings.enableUploadMask,
-  //   state.settings.model,
-  //   state.setFile,
-  //   state.setCustomFile,
-  //   state.runInpainting,
-  //   state.showPrevMask,
-  //   state.hidePrevMask,
-  //   state.imageHeight,
-  //   state.imageWidth,
-  // ]);
 
   const [updateSettings] = useStore((state) => [state.updateSettings]);
   const dev_mode = useStore((state) => state.settings.isDevModeActive);
@@ -94,7 +21,7 @@ const Header = () => {
   };
 
   return (
-    <header className="h-[60px] px-6 py-4 absolute top-[0] flex justify-between items-center z-20 border-b backdrop-filter backdrop-blur-md bg-background/70 w-full ">
+    <header className="h-[60px] px-6 py-4 absolute top-[0] flex justify-between items-start z-20 border-b backdrop-filter backdrop-blur-md bg-background/70 w-full ">
       <div className="flex items-center space-x-2">
         <Switch
           id="development-mode"
